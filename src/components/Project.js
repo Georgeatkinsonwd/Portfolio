5e255be078a741e646bcbcfb6faf36fdacfd6f72
George Atkinson <georgeatkinsonwd@gmail.com>
2023-07-25 14:45:07 +0100

    added logo+pictures and size changes

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,18 +1,26 @@
+import '../styles/project.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub} from '@fortawesome/free-brands-svg-icons'
 
-
-const Project = ({title,img}) => {
+const Project = ({img, title, about, ghLink}) => {
     return(
-        <div>
-                <img style={imgStyle} src={img} alt=""></img>
+        <div className="projectCont">
+            <div className="projectImgs">
+            <img className="upperImg" src={img[0]} alt="filmPic" />
+            <img className="upperImg"  src={img[1]} alt="filmPic" />
+            </div>
+            <div className="projectDesc">
+                <h2>{title}</h2>
+                <p>{about}</p>
+                <a href={ghLink} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="fa-icon" icon={faGithub}/></a>
+                <img className="lowerImg" src={img[2]} alt="filmPic" />
+            
+            </div>
         </div>
     )
 }
 
 
-const imgStyle ={
-    width:"350px",
-    height:"400px"
-}
 
 
 export default Project
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,36 @@
 import '../styles/projects.css'
-import Button from './Button'
+import Project from './Project'
+import filmPic from '../images/filmPic.png'
+import filmHome from '../images/filmHome.png'
+import googleAuth from '../images/googleAuth.png'
+import portfolioWeb from '../images/portfolioWeb.png'
+import portfolioMobile from '../images/portfolioMobile.png'
+import portfolioPage from '../images/portfolioPage.png'
 
 
 
-// Maybe have a carousel of projects rotating 
-
-const Projects = ({onAdd, showProject}) => {
+const Projects = () => {
     return(
-        <div className="projectsBackground">
-            <Button color={showProject ? 'red' : 'green'} onClick={onAdd} text={showProject ? 'Hide Projects' : 'Show Projects'}/> 
-            <div className="imageContainer">
-            
-            </div>
-        </div>
+       <div>
+        <h2>Projects</h2>
+
+        <Project 
+        img={[filmHome,googleAuth, filmPic ]} 
+        title="Film Diary" 
+        about="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id venenatis enim. Maecenas auctor aliquet tincidunt. Maecenas in mauris pulvinar, efficitur lorem in, ornare elit. Integer mollis diam a quam interdum, id laoreet erat sagittis. Morbi cursus egestas sem sed vestibulum. Suspendisse vitae aliquet magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;"
+        ghLink="https://github.com/Georgeatkinsonwd/google-auth-filmList"
+        />
+
+        <Project 
+        img={[portfolioWeb,  portfolioPage, portfolioMobile]} 
+        title="Art Portfolio" 
+        about="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id venenatis enim. Maecenas auctor aliquet tincidunt. Maecenas in mauris pulvinar, efficitur lorem in, ornare elit. Integer mollis diam a quam interdum, id laoreet erat sagittis. Morbi cursus egestas sem sed vestibulum. Suspendisse vitae aliquet magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;"
+        ghLink="https://github.com/Georgeatkinsonwd/Laura-Portfolio"
+        />
+
+       </div>
+        
+       
     )
 }
 
